2f6345aed01fa44b4cf89262c11e4f5e04db0cc3
chipperrip <36035495+chipperrip@users.noreply.github.com>
2018-10-07 12:15:33 +0200

    hy7hyt

--- veke 6/sinesum1_plot.py
+++ veke 6/sinesum1_plot.py
@@ -9,23 +9,41 @@ Use T = 2*pi
 import numpy as np
 import matplotlib.pyplot as plt
 
-def f(t,T):
-    T = 2*np.pi
+def f(t,T=(2*np.pi)):
+
     if 0 < t < T/2:
         return 1
     elif t == T/2:
         return 0
-    else:
+    elif T/2 < t < T:
         return -1
 
-def S(t,n,T):
+def S(t,n,T=(2*np.pi)):
 
     s = 0
     for i in range (1,n+1):
         s += (1/(2*i - 1)) * np.sin((2*(2*i - 1) * np.pi * t)/T)
 
-    return s *(np.pi/4)
+    return s *(4/np.pi)
 
-T = 2*pi
-N = 100
-t_list = np.linspace()
+
+t_list = np.linspace(0, 4*np.pi,20000)
+s_list_1 = S(t_list,1)
+s_list_3 = S(t_list,3)
+s_list_20 = S(t_list, 20)
+s_list_200 = S(t_list, 200)
+f_list = np.asarray([f(t) for t in t_list])
+print(type(f_list))
+print(len(t_list),len(f_list))
+#T = 2*np.pi
+#f_list = np.piecewise(t_list, [(0<t_list<T/2),(t_list==T/2),(T/2 <t_list< T)], [1,0,-1])
+
+plt.title('Plot of sum-of-sines approximations to a function')
+plt.plot(t_list, s_list_1, label='S(t,1)')
+plt.plot(t_list, s_list_3, label='S(t,3)')
+plt.plot(t_list, s_list_20, label='S(t,20)')
+plt.plot(t_list, s_list_200, label='S(t,200)')
+plt.plot(t_list, f_list, label='f(t)')
+plt.xlabel('t')
+plt.legend()
+plt.show()
