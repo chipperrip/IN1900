6e50c5069c742c152e72c6c090047df848285270
chipperrip <36035495+chipperrip@users.noreply.github.com>
2018-10-26 14:11:51 +0200

    v9init

---
+++ veke 9/density_improved.py
@@ -0,0 +1,24 @@
+
+
+def substance (words):
+    substance = ''
+    for word in words(:-1):
+        substance
+
+def read_densities(filename):
+    infile = open(filename, 'r')
+    densities = {}
+    for line in infile:
+        words = line.split()
+        density = float(words[-1])
+
+        if len(words[:-1]) == 2:
+            substance = words[0] +' '+ words[1]
+        else: substance = words[0]
+
+        densities[substance]=density
+
+    infile.close()
+    return densities
+
+densities = read_densities('densities.dat')
